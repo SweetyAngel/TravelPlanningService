5783cb95be136b05745f6a4a8850880ea6857349
AngelinaLY13 <lebed-iurchenko.av@phystech.edu>
2025-05-14 20:37:49 +0300

    backend fix 3 - запускается

--- src/main/java/com/travel/planning/repository/UserRepository.java
+++ src/main/java/com/travel/planning/repository/UserRepository.java
@@ -2,9 +2,8 @@ package com.travel.planning.repository;
 
 import com.travel.planning.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
 import java.util.Optional;
+import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsername(String username);
